f13deb57cbdb984ab590ae97c9fed2c73b320a9a
Geoffry Gambling <geoffrymichael@gmail.com>
2019-09-10 22:32:52 -0700

    Programmatically set up a reusable tableview cell. I like it. Pulling data from our firebase database to show in our cells.

---
+++ VoonChatApp/Controller/MessagesTableviewController.swift
@@ -0,0 +1,89 @@
+//
+//  MessagesTableviewController.swift
+//  VoonChatApp
+//
+//  Created by Geoffry Gambling on 9/10/19.
+//  Copyright © 2019 Geoffry Gambling. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+class MessagesTableviewController: UITableViewController {
+
+    let messageCell = "messageCell"
+    
+    var users = [User]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Cancel", style: .plain, target: self, action: #selector(handleCancel))
+        
+        tableView.register(UserCell.self, forCellReuseIdentifier: messageCell)
+        
+        fetchUser()
+        
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+    
+    func fetchUser() {
+        Database.database().reference().child("users").observe(.childAdded, with: { (snapshot) in
+            
+            if let dictionary = snapshot.value as? [String: AnyObject] {
+                let user = User()
+                
+                user.name = dictionary["name"] as? String
+                user.email = dictionary["email"] as? String
+                self.users.append(user)
+                
+                
+                self.tableView.reloadData()
+                
+                
+            }
+            
+            
+            
+        }, withCancel: nil)
+    }
+    
+    @objc func handleCancel() {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return users.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: messageCell, for: indexPath)
+        
+        let user = users[indexPath.row]
+        
+        cell.textLabel?.text = user.name
+        cell.detailTextLabel?.text = user.email
+    
+        
+        
+        return cell
+        
+    }
+
+}
+
+class UserCell: UITableViewCell {
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: .subtitle, reuseIdentifier: reuseIdentifier)
+        
+        
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
---
+++ VoonChatApp/Model/User.swift
@@ -0,0 +1,16 @@
+//
+//  User.swift
+//  VoonChatApp
+//
+//  Created by Geoffry Gambling on 9/10/19.
+//  Copyright © 2019 Geoffry Gambling. All rights reserved.
+//
+
+import UIKit
+
+class User: NSObject {
+    var name: String?
+    var email: String?
+    
+    
+}
