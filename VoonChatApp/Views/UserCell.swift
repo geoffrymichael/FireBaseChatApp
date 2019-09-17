0c809a19f5ba89a58b801278ea07a990dce8a6e3
Geoffry Gambling <geoffrymichael@gmail.com>
2019-09-16 21:16:57 -0700

    Add and track UserCell class file

---
+++ VoonChatApp/Views/UserCell.swift
@@ -0,0 +1,97 @@
+//
+//  UserCell.swift
+//  VoonChatApp
+//
+//  Created by Geoffry Gambling on 9/16/19.
+//  Copyright © 2019 Geoffry Gambling. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+
+class UserCell: UITableViewCell {
+    
+    
+    var message: Message? {
+        didSet {
+            if let toId = message?.toId {
+                let ref = Database.database().reference().child("users").child(toId)
+                ref.observe(.value, with: { (snapshot) in
+                    if let dictionary = snapshot.value as? [String: AnyObject] {
+                        self.textLabel?.text = dictionary["name"] as? String
+                        if let profileImageUrl = dictionary["profileImageUrl"] as? String {
+                            self.profileImageView.loadImagesUsingCache(url: profileImageUrl)
+                        }
+                    }
+                }, withCancel: nil)
+            }
+            
+            detailTextLabel?.text = message?.text
+            
+            if let seconds = message?.timeStamp?.doubleValue {
+                let timestampDate = NSDate(timeIntervalSince1970: seconds)
+                
+                let dateFormatter = DateFormatter()
+                dateFormatter.dateFormat = "hh:mm:ss a"
+                
+                timeLabel.text = dateFormatter.string(from: timestampDate as Date)
+            }
+            
+            
+        }
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        textLabel?.frame = CGRect(x: 56, y: textLabel!.frame.origin.y, width: textLabel!.frame.width, height: textLabel!.frame.height)
+        
+        detailTextLabel?.frame = CGRect(x: 56, y: detailTextLabel!.frame.origin.y, width: detailTextLabel!.frame.width, height: detailTextLabel!.frame.height)
+    }
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: .subtitle, reuseIdentifier: reuseIdentifier)
+        
+        addSubview(profileImageView)
+        
+        profileImageView.leftAnchor.constraint(equalTo: self.leftAnchor, constant: 8).isActive = true
+        profileImageView.centerYAnchor.constraint(equalTo: self.centerYAnchor)
+        profileImageView.widthAnchor.constraint(equalToConstant: 40).isActive = true
+        profileImageView.heightAnchor.constraint(equalToConstant: 40).isActive = true
+        
+        addSubview(timeLabel)
+        
+        timeLabel.rightAnchor.constraint(equalTo: self.rightAnchor).isActive = true
+        timeLabel.topAnchor.constraint(equalTo: self.topAnchor, constant: 20).isActive = true
+        timeLabel.widthAnchor.constraint(equalToConstant: 100).isActive = true
+        timeLabel.heightAnchor.constraint(equalTo: textLabel!.heightAnchor).isActive = true
+        
+        
+    }
+    
+    let profileImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.layer.cornerRadius = 20
+        imageView.layer.masksToBounds = true
+        imageView.contentMode = .scaleAspectFill
+        
+        return imageView
+        
+    }()
+    
+    let timeLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 12)
+        label.textColor = UIColor.lightGray
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "10/20/2019"
+        
+        return label
+        
+    }()
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
